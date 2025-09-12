f4bddad3899151d5d1d64c08be5b79f4965f4467
Narim Park <skfla2459@gmail.com>
2025-09-12 11:03:48 +0900

    크롬드라이버 자동 설치 변경

--- src/main/java/blogpj/blog/service/VelogCrawlerService.java
+++ src/main/java/blogpj/blog/service/VelogCrawlerService.java
@@ -10,6 +10,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +32,9 @@ public class VelogCrawlerService {
     }
 
     public List<VelogPost> getTopPosts() {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+
+        // WebDriverManager가 자동으로 크롬드라이버 설치/경로 설정
+        WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
@@ -48,14 +53,13 @@ public class VelogCrawlerService {
             int count = Math.min(5, driver.findElements(By.cssSelector("li.PostCard_block__FTMsy")).size());
 
             for (int i = 0; i < count; i++) {
-                // 매번 요소를 다시 찾아 stale 문제 방지
                 WebElement item = driver.findElements(By.cssSelector("li.PostCard_block__FTMsy")).get(i);
 
                 WebElement linkEl = item.findElement(By.cssSelector("a[class*='PostCard_styleLink']"));
                 String link = linkEl.getAttribute("href");
 
                 WebElement imgEl = linkEl.findElement(By.tagName("img"));
-                String title = imgEl.getAttribute("alt").trim().replace(" post", "");;
+                String title = imgEl.getAttribute("alt").trim().replace(" post", "");
 
                 posts.add(new VelogPost(title, link));
             }
